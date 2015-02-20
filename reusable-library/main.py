cadf726a0bd8ed0ab355c44edf5ac82721b75a87
jstanbridge <jstanbridge@fullsail.edu>
2015-02-19 22:22:41 -0500

    convert to int
    added int to numerical values so they would calculate correctly in the
    TeamData functions - took me a while to figure out where they needed to
    go

--- reusable-library/main.py
+++ reusable-library/main.py
@@ -11,77 +11,57 @@ from pages import ResultsPage, FormPage
 class MainHandler(webapp2.RequestHandler):
     def get(self):
 
-        p = ResultsPage()
-
         t = TeamData()
 
-
-        p1 = PlayerData()
-        p1.name = 'One'
-        p1.kills = 6
-        p1.assists = 0
-        p1.deaths = 4
-        p1.heals = 0
-        p1.abs = 2
-        t.add_player(p1)
-
-        p2 = PlayerData()
-        p2.name = 'Two'
-        p2.kills = 12
-        p2.assists = 0
-        p2.deaths = 7
-        p2.heals = 14
-        p2.abs =10
-        t.add_player(p2)
-
         if self.request.GET:
-            pass
-            #p1 = PlayerData()
-            #p1.name = self.request.GET['p1_name']
-            #p1.kills = self.request.GET['p1_kills']
-            #p1.assists = self.request.GET['p1_deaths']
-            #p1.deaths = self.request.GET['p1_assists']
-            #p1.heals = self.request.GET['p1_heals']
-            #p1.abs = self.request.GET['p1_abs']
-            #t.add_player(p1)
+            p1 = PlayerData()
+            p1.name = self.request.GET['p1_name']
+            p1.kills = int(self.request.GET['p1_kills'])
+            p1.assists = int(self.request.GET['p1_deaths'])
+            p1.deaths = int(self.request.GET['p1_assists'])
+            p1.heals = int(self.request.GET['p1_heals'])
+            p1.abs = int(self.request.GET['p1_abs'])
+            t.add_player(p1)
 
-            #p2 = PlayerData()
-            #p2.name = self.request.GET['p2_name']
-            #p2.kills = self.request.GET['p2_kills']
-            #p2.assists = self.request.GET['p2_deaths']
-            #p2.deaths = self.request.GET['p2_assists']
-            #p2.heals = self.request.GET['p2_heals']
-            #p2.abs = self.request.GET['p2_abs']
-            #t.add_player(p2)
+            p2 = PlayerData()
+            p2.name = self.request.GET['p2_name']
+            p2.kills = int(self.request.GET['p2_kills'])
+            p2.assists = int(self.request.GET['p2_deaths'])
+            p2.deaths = int(self.request.GET['p2_assists'])
+            p2.heals = int(self.request.GET['p2_heals'])
+            p2.abs = int(self.request.GET['p2_abs'])
+            t.add_player(p2)
 
-            #p3 = PlayerData()
-            #p3.name = self.request.GET['p3_name']
-            #p3.kills = self.request.GET['p3_kills']
-            #p3.assists = self.request.GET['p3_deaths']
-            #p3.deaths = self.request.GET['p3_assists']
-            #p3.heals = self.request.GET['p3_heals']
-            #p3.abs = self.request.GET['p3_abs']
-            #t.add_player(p3)
+            p3 = PlayerData()
+            p3.name = self.request.GET['p3_name']
+            p3.kills = int(self.request.GET['p3_kills'])
+            p3.assists = int(self.request.GET['p3_deaths'])
+            p3.deaths = int(self.request.GET['p3_assists'])
+            p3.heals = int(self.request.GET['p3_heals'])
+            p3.abs = int(self.request.GET['p3_abs'])
+            t.add_player(p3)
 
-            #p4 = PlayerData()
-            #p4.name = self.request.GET['p4_name']
-            #p4.kills = self.request.GET['p4_kills']
-            #p4.assists = self.request.GET['p4_deaths']
-            #p4.deaths = self.request.GET['p4_assists']
-            #p4.heals = self.request.GET['p4_heals']
-            #p4.abs = self.request.GET['p4_abs']
-            #t.add_player(p4)
+            p4 = PlayerData()
+            p4.name = self.request.GET['p4_name']
+            p4.kills = int(self.request.GET['p4_kills'])
+            p4.assists = int(self.request.GET['p4_deaths'])
+            p4.deaths = int(self.request.GET['p4_assists'])
+            p4.heals = int(self.request.GET['p4_heals'])
+            p4.abs = int(self.request.GET['p4_abs'])
+            t.add_player(p4)
 
-            #p5 = PlayerData()
-            #p5.name = self.request.GET['p5_name']
-            #p5.kills = self.request.GET['p5_kills']
-            #p5.assists = self.request.GET['p5_deaths']
-            #p5.deaths = self.request.GET['p5_assists']
-            #p5.heals = self.request.GET['p5_heals']
-            #p5.abs = self.request.GET['p5_abs']
-            #t.add_player(p5)
+            p5 = PlayerData()
+            p5.name = self.request.GET['p5_name']
+            p5.kills = int(self.request.GET['p5_kills'])
+            p5.assists = int(self.request.GET['p5_deaths'])
+            p5.deaths = int(self.request.GET['p5_assists'])
+            p5.heals = int(self.request.GET['p5_heals'])
+            p5.abs = int(self.request.GET['p5_abs'])
+            t.add_player(p5)
 
-            #p.body = t.player_list() + t.calc_kills() + t.calc_deaths() + t.calc_abs() + t.calc_heals()
+            p = ResultsPage()
+            p.body = t.player_list() + t.calc_kills() +  t.calc_deaths() + t.calc_abs() + t.calc_heals()
+            self.response.write(p.print_out())
         else:
             p = FormPage()
             self.response.write(p.print_out())
