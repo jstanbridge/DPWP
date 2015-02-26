9a79ee8723533e2217b111135c00f3e777677847
jstanbridge <jstanbridge@fullsail.edu>
2015-02-26 16:58:05 -0500

    Page class
    added Page class shell

--- dynamic-site/page.py
+++ dynamic-site/page.py
@@ -3,3 +3,18 @@ James Stanbridge
 26 Feb 2014
 Dynamic Site
 """
+
+class Page(object):
+    def __init__(self):
+        self._head = '''
+
+        '''
+
+        self._body = '''
+        '''
+
+        self._close = '''
+        '''
+
+    def print_out(self):
+        return self._head + self._body + self._close
