d0824c2a6e9b08c1136e8534cebe0ce4a4c49fd0
jstanbridge <jstanbridge@fullsail.edu>
2015-02-26 17:17:30 -0500

    placeholder links
    added placeholder links to the body in the page class

--- dynamic-site/page.py
+++ dynamic-site/page.py
@@ -11,6 +11,11 @@ class Page(object):
         '''
 
         self._body = '''
+        <a href="1">1</a>
+        <a href="2">2</a>
+        <a href="3">3</a>
+        <a href="4">4</a>
+        <a href="5">5</a>
         '''
 
         self._close = '''
