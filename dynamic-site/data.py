e31256e629786d10d5b789bd5f10f7e220d605eb
jstanbridge <jstanbridge@fullsail.edu>
2015-02-26 18:20:57 -0500

    data object
    added data object and updated attribute titles

--- dynamic-site/data.py
+++ dynamic-site/data.py
@@ -3,3 +3,48 @@ James Stanbridge
 26 Feb 2014
 Dynamic Site
 """
+
+class Data(self):
+    def __init__(self):
+
+        cat = Movie()
+        cat.title = 'The Cat Returns'
+        cat.time = 95
+        cat.studio = 'Studio Ghibli'
+        cat.year = 2002
+        cat.genre = 'Fantasy'
+        cat.director = 'Hiroyuki Morita'
+
+        time = Movie()
+        time.title = 'The Cat Returns'
+        time.time = 95
+        time.studio = 'Studio Ghibli'
+        time.year = 2002
+        time.genre = 'Fantasy'
+        time.director = 'Hiroyuki Morita'
+
+        sword = Movie()
+        sword.title = 'The Cat Returns'
+        sword.time = 95
+        sword.studio = 'Studio Ghibli'
+        sword.year = 2002
+        sword.genre = 'Fantasy'
+        sword.director = 'Hiroyuki Morita'
+
+        cent = Movie()
+        cent.title = 'The Cat Returns'
+        cent.time = 95
+        cent.studio = 'Studio Ghibli'
+        cent.year = 2002
+        cent.genre = 'Fantasy'
+        cent.director = 'Hiroyuki Morita'
+
+        spirit = Movie()
+        spirit.title = 'The Cat Returns'
+        spirit.time = 95
+        spirit.studio = 'Studio Ghibli'
+        spirit.year = 2002
+        spirit.genre = 'Fantasy'
+        spirit.director = 'Hiroyuki Morita'
+
+        self.list = [cat, time, sword, cent, spirit]
