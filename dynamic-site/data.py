578ae8a6430385d9dd9942ed0d8de9bbddf8c6e5
jstanbridge <jstanbridge@fullsail.edu>
2015-02-26 18:23:45 -0500

    time and year
    updated all times and years in data object

--- dynamic-site/data.py
+++ dynamic-site/data.py
@@ -17,25 +17,25 @@ class Data(self):
 
         time = Movie()
         time.title = 'The Girl Who Leapt Through Time'
-        time.time = 95
+        time.time = 98
         time.studio = 'Studio Ghibli'
-        time.year = 2002
+        time.year = 2006
         time.genre = 'Fantasy'
         time.director = 'Hiroyuki Morita'
 
         sword = Movie()
         sword.title = 'Sword of the Stranger'
-        sword.time = 95
+        sword.time = 98
         sword.studio = 'Studio Ghibli'
-        sword.year = 2002
+        sword.year = 2007
         sword.genre = 'Fantasy'
         sword.director = 'Hiroyuki Morita'
 
         cent = Movie()
         cent.title = '5 Centimeters Per Second'
-        cent.time = 95
+        cent.time = 62
         cent.studio = 'Studio Ghibli'
-        cent.year = 2002
+        cent.year = 2007
         cent.genre = 'Fantasy'
         cent.director = 'Hiroyuki Morita'
 
@@ -43,7 +43,7 @@ class Data(self):
         spirit.title = 'Origin: Spirits of the Past'
         spirit.time = 95
         spirit.studio = 'Studio Ghibli'
-        spirit.year = 2002
+        spirit.year = 2006
         spirit.genre = 'Fantasy'
         spirit.director = 'Hiroyuki Morita'
 
