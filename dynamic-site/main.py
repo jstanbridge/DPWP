9b7263a9a34ee571b7b8b10b5d62b05bc1ce9aea
jstanbridge <jstanbridge@fullsail.edu>
2015-02-26 22:36:10 -0500

    content page set/get
    added setter and getters for each attribute for the data objects to the
    contentpage subclass

--- dynamic-site/main.py
+++ dynamic-site/main.py
@@ -5,13 +5,14 @@ Dynamic Site
 """
 
 import webapp2
-from page import Page
+from page import Page, ContentPage
 from data import Movie, Data
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
 
         p = Page()
+        c = ContentPage()
         m = Movie()
         d = Data()
 
@@ -31,6 +32,15 @@ class MainHandler(webapp2.RequestHandler):
 
         else:
             pass
+
+        c._title = m.title
+        c._image = m.image
+        c._time = m.time
+        c._studio = m.studio
+        c._year = m.year
+        c._genre = m.genre
+        c._director = m.director
+        print c._title
 
         p.item = '<img src="' + m.image + '" />' + m.title + str(m.time) + m.studio + str(m.year) + m.genre + m.director
 
--- dynamic-site/page.py
+++ dynamic-site/page.py
@@ -38,3 +38,76 @@ class Page(object):
 class ContentPage(Page):
     def __init__(self):
         super(ContentPage, self).__init__()
+        self._div_start = '<div id="info">'
+        self._div_close = '</div>'
+
+        self._title = ''
+
+        @property
+        def title(self):
+            pass
+
+        @title.setter
+        def title(self, new_title):
+            self._title = new_title
+
+        self._image = ''
+
+        @property
+        def image(self):
+            pass
+
+        @image.setter
+        def image(self, new_image):
+            self._image = new_image
+
+        self._time = 0
+
+        @property
+        def time(self):
+            pass
+
+        @time.setter
+        def time(self, new_time):
+            self._time = new_time
+
+        self._genre = ''
+
+        @property
+        def genre(self):
+            pass
+
+        @genre.setter
+        def genre(self, new_genre):
+            self._genre = new_genre
+
+        self._director = ''
+
+        @property
+        def director(self):
+            pass
+
+        @director.setter
+        def director(self, new_director):
+            self._director = new_director
+
+        self._year  = 0
+
+        @property
+        def year(self):
+            pass
+
+        @year.setter
+        def year(self, new_year):
+            self._year = new_year
+
+        self._studio = ''
+
+        @property
+        def studio(self):
+            pass
+
+        @studio.setter
+        def studio(self, new_studio):
+            self._studio = new_studio
+
